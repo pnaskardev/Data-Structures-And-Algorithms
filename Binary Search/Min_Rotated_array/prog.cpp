0de3e2011cbbafeac0d840e38c9d3d80e0ca4631
Priyanshu Naskar <71266237+pnaskardev@users.noreply.github.com>
2021-10-12 11:38:13 +0530

    main

--- Binary Search/Min_Rotated_array/prog.cpp
+++ Binary Search/Min_Rotated_array/prog.cpp
@@ -3,11 +3,25 @@
 using namespace std;
 int min_rot(vector<int>arr)
 {
-
+    int start=0,high=arr.size()-1,ans=-1;
+    while(start<=high)
+    {
+        int mid =start+(high-start)/2;
+        if(arr[mid]<arr[high])
+        {
+            ans=mid;
+            high=mid;
+        }
+        else
+        {
+            start=mid+1;
+        }
+    }
+    return arr[ans];
 }
 int main(void)
 {
     vector<int>arr={7,8,9,1,2,4,5,6};
     cout<<min_rot(arr)<<endl;
-    
+
 }
