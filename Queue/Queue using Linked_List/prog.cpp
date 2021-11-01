49f2c5f77436071fe36124918d890d2c705e3165
Priyanshu Naskar <71266237+pnaskardev@users.noreply.github.com>
2021-11-01 16:05:30 +0530

    main

--- Queue/Queue using Linked_List/prog.cpp
+++ Queue/Queue using Linked_List/prog.cpp
@@ -6,10 +6,10 @@ class Node
     public:
     int data;
     Node *next;
-
 };
 void enqueue(int x);
 int dequeue(Node *&first,Node *&last);
+void display(Node *first);
 int main(void)
 {
     Node *first=NULL,*last=NULL;
@@ -40,5 +40,27 @@ void enqueue(int x,Node *&first, Node *&last)
 }
 int dequeue(Node *&first,Node *&last)
 {
-    
+    int x=-1;
+    Node *t ;
+    if(first==NULL)
+    {
+        cout<<"Queue is empty"<<endl;
+    }
+    else
+    {
+        x=first->data;
+        t=first;
+        first=first->next;
+        delete(t);
+    }
+}
+void display(Node *first)
+{
+    Node *p=first;
+    while(p!=NULL)
+    {
+        cout<<p->data<<" ";
+        p=p->next;
+    }
+    cout<<endl;
 }
