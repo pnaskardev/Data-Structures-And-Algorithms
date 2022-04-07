beb4cc3142c8ff537ae61b411ed3b0d8181f6b72
pnaskardev <pnaskardev@gmail.com>
2022-04-07 12:48:16 +0530

    recursive dfs added

--- Graph Traversals/BFS_DFS_CPP.cpp
+++ Graph Traversals/BFS_DFS_CPP.cpp
@@ -2,7 +2,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void dfs(int u, int A[][8], int n)
+void recursive_dfs(int u, int A[][8], int n)
+{
+    static int visited[8] {0};
+ 
+    if (visited[u] == 0)
+    {
+        cout << u << ", " << flush;
+        visited[u] = 1;
+        for (int v=1; v<n; v++)
+        {
+            if (A[u][v] == 1 && visited[v] == 0)
+            {
+                recursive_dfs(v, A, n);
+            }
+        }
+    }
+}
+
+void iterative_dfs(int u, int A[][8], int n)
 {
     int visited[8] {0};
     stack<int> stk;
